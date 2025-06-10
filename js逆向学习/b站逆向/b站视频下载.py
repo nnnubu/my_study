a27cebd4e704be63239f75e14e87d97cddab2938
nnnubu <148740267+nnnubu@users.noreply.github.com>
2025-06-10 23:13:15 +0800

    Update b站视频下载.py

--- js逆向学习/b站逆向/b站视频下载.py
+++ js逆向学习/b站逆向/b站视频下载.py
@@ -13,10 +13,11 @@ def generate_unique_filename(path, title, extension):
     return title
 
 
-url = 'https://www.bilibili.com/video/BV1QVSJYzEYf/?spm_id_from=333.788.videopod.sections&vd_source=9cb26b93127feca8518ea39506132c11&p=7'
-# url = 'https://www.bilibili.com/video/BV1QVSJYzEYf/?spm_id_from=333.788.videopod.sections&vd_source=9cb26b93127feca8518ea39506132c11'
+url = ''
+
+cookie = ''
+
 header = {
-    "cookie": "buvid3=A07A2F16-A326-C9EA-D3C7-9819F05A3CA425846infoc; b_nut=1748624025; _uuid=A8108F6EB-B8EB-E84A-9EA6-DA2FB431088FC25387infoc; buvid_fp=858a4450213f5c2b086f64826eb0abc4; header_theme_version=CLOSE; enable_web_push=DISABLE; enable_feed_channel=ENABLE; home_feed_column=5; browser_resolution=2133-1083; buvid4=13B7A145-B277-5666-9361-2FAB63C42AD789708-024042510-A29gAFm6qKCDuygPoa4PQA%3D%3D; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDg4ODMyMjcsImlhdCI6MTc0ODYyMzk2NywicGx0IjotMX0.HiSo4-AhW3Gm1ops2CXBLCV7X1355GhN-gcv5HPLdoY; bili_ticket_expires=1748883167; SESSDATA=1c165b9d%2C1764177084%2C0f187%2A52CjCBwt4i4kYSYGYDBoN5ZNzAnqdJ1ceVn458ItYwWYT7YaReZCT3E_56DCRD6cZuIx4SVk83WlJkVjc0dnFDajRQMnBFMjZ3eE9HUWVybWNZR21abHhYQUg2dVc0N3R0SVRFRmRmRjZ5V0xwaTh1RWxCV3ZGSXluTGpZdnRqQlFDbVZ5YjA1WkxnIIEC; bili_jct=d1bb4aed04aa0753130eb083f80eac31; DedeUserID=430970446; DedeUserID__ckMd5=6b1ce26d8638a884; b_lsid=CEBC1BCD_197226A1D3D; bmg_af_switch=1; bmg_src_def_domain=i1.hdslb.com; sid=6ojsagri; CURRENT_FNVAL=4048; rpdid=|(J|)lYJ|)|)0J'u~R)Ykm|mm; CURRENT_QUALITY=80",
     "referer": "https://www.bilibili.com/",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0",
 }
